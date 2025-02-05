e7b63a80be17cdf3157e73087a8e4bfe6b177108
chafidz05 <chafidz0505@gmail.com>
2025-02-06 00:47:27 +0700

    v1.5 Layout, Nav, Sidebar

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex h-screen items-center justify-center bg-amber-100">
-      <h1 className="text-3xl font-bold text-brand">
-        StoreIt - The only storage solution you need.
-      </h1>
-    </div>
-  );
-}
