75abed9bcd47d15bd34954db95008af637eccaa6
chafidz05 <chafidz0505@gmail.com>
2025-02-06 16:24:49 +0700

    v1.6 Mobile Navigation

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+"use client";
 
-const MobileNavigation = () => {
-    return <div>MobileNavigation</div>;
-};
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import React, { useState } from "react";
+import { usePathname } from "next/navigation";
+import { Separator } from "@radix-ui/react-separator";
+import { navItems } from "@/constants";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import FileUploader from "@/components/FileUploader";
+import { signOutUser } from "@/lib/actions/user.actions";
+
+interface Props {
+    ownerId: string,
+    accountId: string,
+    fullName: string;
+    avatar: string;
+    email: string;
+}
+
+const MobileNavigation = ({
+    ownerId,
+    accountId,
+    fullName,
+    avatar,
+    email,
+}: Props) => {
+    const [open, setOpen] = useState(false);
+    const pathname = usePathname();
+
+    return (
+        <header className="mobile-header">
+
+        </header>
+    )
+}
 
 export default MobileNavigation;
