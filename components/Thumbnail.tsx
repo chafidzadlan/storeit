0176e5d7b950fd07d17da0cadb05d1adb6225013
chafidz05 <chafidz0505@gmail.com>
2025-02-08 14:49:18 +0700

    v2.4 File Upload

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -3,37 +3,36 @@ import Image from "next/image";
 import { cn, getFileIcon } from "@/lib/utils";
 
 interface Props {
-    type: string;
-    extension: string;
-    url?: string;
-    imageClassName?: string;
-    className?: string;
+  type: string;
+  extension: string;
+  url?: string;
+  imageClassName?: string;
+  className?: string;
 }
 
-const Thumbnail = ({
-    type,
-    extension,
-    url = "",
-    imageClassName,
-    className,
+export const Thumbnail = ({
+  type,
+  extension,
+  url = "",
+  imageClassName,
+  className,
 }: Props) => {
-    const isImage = type === "image" && extension !== "svg";
+  const isImage = type === "image" && extension !== "svg";
 
-    return (
-        <figure className={cn("thumbnail", className)}>
-            <Image
-                src={isImage ? url : getFileIcon(extension, type)}
-                alt="thumbnail"
-                width={100}
-                height={100}
-                className={cn(
-                    "size-8 object-contain",
-                    imageClassName,
-                    isImage && "thumbnail-image",
-                )}
-            />
-        </figure>
-    );
+  return (
+    <figure className={cn("thumbnail", className)}>
+      <Image
+        src={isImage ? url : getFileIcon(extension, type)}
+        alt="thumbnail"
+        width={100}
+        height={100}
+        className={cn(
+          "size-8 object-contain",
+          imageClassName,
+          isImage && "thumbnail-image",
+        )}
+      />
+    </figure>
+  );
 };
-
 export default Thumbnail;
